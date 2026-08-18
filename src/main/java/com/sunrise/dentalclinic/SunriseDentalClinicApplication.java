773d3817a81242cdffbc41dee39de69752971e4e
shene <nishelashene@gmail.com>
2026-08-18 13:20:06 +0530

    Initial commit

---
+++ src/main/java/com/sunrise/dentalclinic/SunriseDentalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sunrise.dentalclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseDentalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseDentalClinicApplication.class, args);
+	}
+
+}
